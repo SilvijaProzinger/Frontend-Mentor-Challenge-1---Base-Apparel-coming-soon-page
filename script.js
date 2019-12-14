14b518556974da8db9cf570e087003627bd76259
Silvija Prozinger <37271365+SilvijaProzinger@users.noreply.github.com>
2019-12-14 19:22:19 +0100

    design change and input checking function

---
+++ script.js
@@ -0,0 +1,21 @@
+const submit = document.getElementById('submit');
+
+const checkEmail = () => {
+	event.preventDefault();
+	let form = document.forms['form']['email'].value;
+	const email = /\S+@\S+\.\S+/;
+	// Validate input
+	if (form === '' || !form.match(email)){
+		document.getElementById('inputField').classList.remove('inactive');
+		document.getElementById('inputField').classList.add('empty-field');
+		document.getElementById('err').style.display ='inline';
+	} else if (form.match(email)) {
+		form = '';
+		document.getElementById('err').style.display ='none';
+		document.getElementById('inputField').classList.add('inactive');
+		document.getElementById('inputField').classList.remove('empty-field');
+  	}
+}
+  
+submit.addEventListener('click', checkEmail)
+
