89ee6804902e68acce994dc908820289ca9c8e33
Silvija Prozinger <37271365+SilvijaProzinger@users.noreply.github.com>
2019-12-15 16:46:44 +0100

    changed input placeholder background to be able to show error icon

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const checkEmail = () => {
 	let form = document.forms['form']['email'].value;
 	const email = /\S+@\S+\.\S+/;
 	// Validate input
-	if (form === '' || !form.match(email)){
+	if (!form.match(email) || form === ''){
 		document.getElementById('inputField').classList.remove('inactive');
 		document.getElementById('inputField').classList.add('empty-field');
 		document.getElementById('err').style.display ='inline';
